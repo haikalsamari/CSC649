5edfb65ce928b7bc2c00efed8dfe6a30e7d974cb
CokeSodaWine <87024651+CokeSodaWine@users.noreply.github.com>
2024-01-16 16:17:25 +0800

    encoding turnover dataset

--- Development/test.py
+++ Development/test.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-read_data = pd.read_csv("../Datasets/turnover.csv")
+st.title("Welcome to CSC649")
+st.header("This is execrise on the implementation of interface development via StreamLit")
+st.write("This is the dataset on iris")
+
+read_data = pd.read_csv("../Datasets/turnover.csv", encoding="latin-1")
 read_data
